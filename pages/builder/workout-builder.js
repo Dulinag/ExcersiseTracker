6666d5477e4b24cc55f2a92c5b78ad4205446a7f
Bennett Kautz <92739107+bkautz0824@users.noreply.github.com>
2023-09-20 19:58:30 -0400

    heeeem

--- pages/builder/workout-builder.js
+++ pages/builder/workout-builder.js
@@ -1,9 +1,12 @@
 import React from 'react'
+// import { Button } from 'shadcn-ui';
+
 
 function FirstPage() {
   return (
     <div>
-        <h1>Helloooo</h1>
+        <h1 className="text-3xl font-bold underline m-4 flex justify-center">Helloooo</h1>
+        {/* <Button>Click me</Button> */}
     </div>
   )
 }
